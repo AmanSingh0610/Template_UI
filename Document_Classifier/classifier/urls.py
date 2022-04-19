6f157fdcc5c2e42e5278be052e167381f1e0b04c
amankrsingh2000 <amankrsingh2110@gmail.com>
2022-04-19 15:05:22 +0530

    Added Heroku Deployment

--- Document_Classifier/classifier/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Start.as_view()),
-    path('index/', views.Index.as_view()),
-    path('detect/<int:id>',views.Detect.as_view()),
-    path('history/', views.History.as_view()),  
-]
